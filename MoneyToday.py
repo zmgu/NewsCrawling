cb0e627424d1fe6455aeb4d5e0789ff4801684e1
JeonMinHyeok <manggu94@gmail.com>
2023-08-23 13:47:08 +0900

    MoneyToday

--- MoneyToday.py
+++ MoneyToday.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 from bs4 import BeautifulSoup
 import pymysql
@@ -13,9 +16,11 @@ def moneyToday():
     cur = conn.cursor()
 
     # selenium 기본 설정
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
-    # 최신 날짜 불러오기
+    # 최신 데이터 날짜
     date_sql = 'SELECT DATE FROM NEWS_ARTICLES WHERE PRESS = "머니투데이" ORDER BY DATE DESC LIMIT 1'
     cur.execute(date_sql)
     ck = cur.fetchone()
@@ -31,15 +36,19 @@ def moneyToday():
     # 이전 데이터 크롤링 완료 체크
     already_data = False
 
-    for page in range(1, 51):
+    for page in range(1, 5):
         if already_data is True:
             break
+
         url = f'https://news.mt.co.kr/newsList.html?type=1&comd=&pDepth=news&pDepth1=politics&pDepth2=Ptotal&page={page}'
-        driver.implicitly_wait(10)
         driver.get(url)
         print(f'머니투데이 {page}페이지 이동')
 
-        # 크롤링 시작
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#content > ul > li')))
+        except TimeoutException:
+            continue
+
         news_list = driver.find_elements(
             By.CSS_SELECTOR, '#content > ul')
 
@@ -57,7 +66,7 @@ def moneyToday():
                     except NoSuchElementException:
                         img_url = ''
 
-                    # 추출한 URL 크롤링 시작
+                    # BeautifulSoup
                     resource = requests.get(href)
                     soup = BeautifulSoup(resource.text, 'html.parser')
 
@@ -84,7 +93,7 @@ def moneyToday():
                     # 크롤링 데이터 리스트
                     insert_list = [date, title, content, '머니투데이', href, img_url]
                     data_list.append(insert_list)
-                    print(insert_list)
+
                 except NoSuchElementException:
                     continue
 
